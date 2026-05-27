fc6c89ea30bbfcd2def8b8913ff7fea0ee0255de
User_at3am <prajapatiaman18113@gmail.com>
2026-05-27 23:04:06 +0530

    Initial Commit

---
+++ Recipe With Grocery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe With Grocery
+//
+//  Created by apple on 27/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe With Grocery/Recipe_With_GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_With_GroceryApp.swift
+//  Recipe With Grocery
+//
+//  Created by apple on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_With_GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
